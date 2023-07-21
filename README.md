7c5ae5c6bb96ba047a58718cbcf413a1f7bf1d66
Mr. T <89462237+EndlessFractal@users.noreply.github.com>
2023-07-21 10:19:12 +0100

    Upload script and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # mailHound
-Email analysis with python
+
+This tool is designed to analyze the headers and content of email files and provide valuable information. It can extract sender and recipient information, important header details, URLs, and attachments from an email file in `.eml` or `.msg` format.
+
+## Installation
+
+To use the mailHound, follow these steps:
+
+1. Clone the repository using the following command:
+
+```bash
+git clone https://github.com/your-username/email-header-analysis.git
+```
+
+2. Navigate to the cloned directory:
+
+```bash
+cd mailHound
+```
+
+## Usage
+
+To analyze an email file, simply run the `mailHound.py` script with the path to the email file as an argument:
+
+```bash
+python mailHound.py path/to/email_file.eml
+```
+
+Replace `path/to/email_file.eml` with the actual path to your email file.
+
+### Supported Formats
+
+The tool supports two email file formats: `.eml` and `.msg`. If your email file has a different extension or is in another format, the tool will raise an error.
+
+### Analysis Report
+
+After running the script, the tool will generate a detailed analysis report for the provided email file. The report will include the following information:
+
+- Sender Name and Email
+- Recipients (TO, CC, BCC)
+- Reply-To
+- Subject
+- Date
+- Message-ID
+- Received
+- Authentication Results
+- URLs found in the email content
+- Attachments & Files and their respective sizes
+
+Note: If the email file does not contain any URLs or attachments, the report will indicate this accordingly.
+
+### Example
+
+```bash
+Email Header Analysis Report:
+
+Sender Name: Group Marketing Online Accounts Payable
+Sender Email: Accounts.Payable@groupmarketingonline.icu
+Subject: Group Marketing Online Direct Credit Advice - michael.ascot@swiftspend.finance
+Date: Sun, 28 Jun 2020 17:01:33 -0500
+Received: from SG2PR01MB4517.apcprd01.prod.exchangelabs.com(YYYY:YYYY:YYYY:YYYY:YYYY:YYYY) by SG2PR01MB3173.apcprd01.prod.exchangelabs.com withHTTPS; Sun, 28 Jun 2020 22:01:56 +0000
+TO:
+  - Michael Ascot <michael.ascot@swiftspend.finance>
+Authentication Results:
+  - spf=pass (sender IP is X.X.X.X)smtp.mailfrom=BRAEMARHOWELLS.COM; dkim=none (message not signed)header.d=none;dmarc=none action=noneheader.from=groupmarketingonline.icu;compauth=softpass reason=202
+# of URLs: 14
+URLs:
+  - https://www.facebook.com/GroupMarketingOnline
+  - https://www.youtube.com/user/GroupMarketingOnline
+  - https://www.linkedin.com/company/GroupMarketingOnline
+  - https://twitter.com/GroupMarketingOnline
+  - https://www.instagram.com/GroupMarketingOnline/
+  - https://pinterest.com/GroupMarketingOnline/
+  - http://schemas.microsoft.com/office/2004/12/omml
+  - http://www.w3.org/TR/REC-html40
+  - https://www.facebook.com/GroupMarketingOnline
+  - https://www.youtube.com/user/GroupMarketingOnline
+  - https://www.linkedin.com/company/GroupMarketingOnline
+  - https://twitter.com/GroupMarketingOnline
+  - https://www.instagram.com/GroupMarketingOnline/
+  - https://pinterest.com/GroupMarketingOnline/
+# of Attachments & Files: 9
+Attachments & Files:
+  - Name: image001.png, Size: 25.88 KB
+  - Name: image003.png, Size: 794 B
+  - Name: image004.png, Size: 984 B
+  - Name: image005.png, Size: 921 B
+  - Name: image006.png, Size: 756 B
+  - Name: image007.png, Size: 819 B
+  - Name: image008.png, Size: 776 B
+  - Name: image009.png, Size: 21.19 KB
+  - Name: Direct Credit Advice.html, Size: 515 B
+```
+
+## Contributing
+
+If you find any issues with this tool or have suggestions for improvements, feel free to open an issue or submit a pull request on!
---
+++ mailHound.py
@@ -0,0 +1,163 @@
+import argparse
+import re
+from email import message_from_binary_file, policy, utils
+
+
+def extract_urls_from_part(part):
+    """Extracts URLs from a text/html or text/plain email part."""
+    content_type = part.get_content_type()
+    if content_type in ("text/html", "text/plain"):
+        content = part.get_content()
+        return re.findall(r"(?P<url>https?://[^\s\">\]\']+)", content)
+    return []
+
+
+def extract_recipients(header_value):
+    """Extracts recipients' names and email addresses from the email header."""
+    recipients = []
+    if header_value:
+        recipient_list = header_value.split(",")
+        for recipient in recipient_list:
+            recipient_name, recipient_email = utils.getaddresses([recipient.strip()])[0]
+            # Decode non-ASCII names in the recipient field
+            recipient_name = recipient_name.encode('latin-1').decode('utf-8')
+            recipients.append(f"{recipient_name} <{recipient_email}>")
+    return recipients
+
+
+def decode_attachment(part):
+    """Extracts information about an attachment from an email part."""
+    content_disposition = part.get_content_disposition()
+    content_id = part.get("Content-ID")
+
+    if content_id:
+        # If "Content-ID" is present, treat it as an embedded image
+        image_name = content_id.strip("<>").split("@")[0]
+        image_data = part.get_content()
+        image_size = len(image_data)
+        return {"Name": image_name, "Size": image_size}
+
+    if content_disposition and "attachment" in content_disposition.lower():
+        attachment_name = part.get_filename()
+        attachment_size = len(part.get_content())
+        return {"Name": attachment_name, "Size": attachment_size}
+
+    return None
+
+
+def analyze_email_headers(file_path, email_policy):
+    """Analyzes the email headers and content from the specified file."""
+    with open(file_path, "rb") as file:
+        email_message = message_from_binary_file(file, policy=email_policy)
+
+    # Extract sender's name and email address
+    sender_name, sender_email = utils.getaddresses([email_message["From"]])[0]
+
+    # Extract recipients
+    recipients = {
+        "TO": extract_recipients(email_message["To"]),
+        "CC": extract_recipients(email_message["Cc"]),
+        "BCC": extract_recipients(email_message["Bcc"]),
+    }
+
+    # Extract other headers
+    headers = ["Reply-To", "Subject", "Date", "Message-ID", "Received"]
+    header_data = {header: email_message.get(header, None) for header in headers}
+
+    # Extract authentication results
+    authentication_results = email_message.get_all("Authentication-Results", [])
+
+    # Extract URLs and attachments from the email content
+    urls = []
+    attachments = []
+    for part in email_message.walk():
+        urls += extract_urls_from_part(part)
+        attachment = decode_attachment(part)
+        if attachment:
+            attachments.append(attachment)
+
+    # Prepare results dictionary
+    results = {
+        "Sender Name": sender_name,
+        "Sender Email": sender_email,
+        **header_data,
+        **recipients,
+        "Authentication Results": authentication_results,
+        "# of URLs": len(urls),
+        "URLs": urls,
+        "# of Attachments & Files": len(attachments),
+        "Attachments & Files": attachments,
+    }
+
+    return results
+
+
+def format_size(size_in_bytes):
+    """Formats the size in bytes to a human-readable format (e.g., KB, MB)."""
+    for unit in ['B', 'KB', 'MB', 'GB']:
+        if size_in_bytes < 1024:
+            return f"{size_in_bytes:.0f} {unit}" if size_in_bytes == int(size_in_bytes) else f"{size_in_bytes:.2f} {unit}"
+        size_in_bytes /= 1024
+
+
+def generate_report(results):
+    """Generates a report based on the analysis results."""
+    report = """
+                 _ _ _   _                       _
+                (_) | | | |                     | |
+ _ __ ___   __ _ _| | |_| | ___  _   _ _ __   __| |
+| '_ ` _ \ / _` | | |  _  |/ _ \| | | | '_ \ / _` |
+| | | | | | (_| | | | | | | (_) | |_| | | | | (_| |
+|_| |_| |_|\__,_|_|_\_| |_/\___/ \__,_|_| |_|\__,_|
+
+by Endless Fractal\n\n"""
+    if results:
+        for key, value in results.items():
+            if value:  # Check if the value is not empty
+                if isinstance(value, list):
+                    report += f"{key}:\n"
+                    for item in value:
+                        if isinstance(item, dict):
+                            name = item.get('Name', 'N/A')
+                            size = item.get('Size', 'N/A')
+                            size_formatted = format_size(size)
+                            report += f"  - Name: {name}, Size: {size_formatted}\n"
+                        else:
+                            report += f"  - {item}\n"
+                else:
+                    report += f"{key}: {value}\n"
+    else:
+        report += "No warnings found in the email headers."
+
+    return report
+
+
+def main():
+    """Main entry point for the email header analysis script."""
+    parser = argparse.ArgumentParser(description="Analyze email headers")
+    parser.add_argument("file_path", type=str, help="Path to the email file")
+    args = parser.parse_args()
+
+    file_path = args.file_path
+
+    # Determine the policy based on the file extension
+    if file_path.lower().endswith(".eml"):
+        email_policy = policy.default
+    elif file_path.lower().endswith(".msg"):
+        email_policy = policy.SMTP
+    else:
+        print("Unsupported email file format.")
+        return
+
+    try:
+        results = analyze_email_headers(file_path, email_policy)
+        report = generate_report(results)
+        print(report)
+    except FileNotFoundError:
+        print("Error: The specified email file was not found.")
+    except Exception as e:
+        print(f"An error occurred during email analysis: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
